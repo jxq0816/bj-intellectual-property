ffd9d3d229873d14085e3769f1a8b98336e4fb60
姜兴琪 <xingqijiang@qq.com>
2020-02-25 22:04:07 +0800

    获取URL

---
+++ gene_chips_detail.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import time
+import os
+import csv
+from selenium import webdriver
+from datetime import datetime
+
+
+def fun(browser, file, no, name, url):
+    browser.get(url)
+    summary=browser.find_element_by_class_name('patent_b').find_element_by_class_name('c').text.strip()
+    rs=no+'|'+name+'|'+summary
+    trs=browser.find_element_by_id('inid').find_elements_by_tag_name('tr')
+    i=1
+    for tr in trs:
+        if i==3 or i==4 or i==5:
+            td=tr.find_element_by_tag_name('td')
+            rs=rs+'|'+td.text
+        i=i+1
+    rs=rs+'|'+browser.find_element_by_id('claims').text.replace(" ", "").replace("\n", "")+'\n'
+    file.write(rs)
+
+if __name__ == '__main__':
+    chrome_drive = '/Users/jiangxingqi/Sina/chromedriver'
+    browser = webdriver.Chrome(executable_path=chrome_drive)
+    file = open('rs.csv', 'w')
+    sFileName = 'url_list.csv'
+    with open(sFileName, newline='', encoding='UTF-8') as csvfile:
+        rows = csv.reader(csvfile)
+        for row in rows:
+            no=row[0]
+            name=row[1]
+            url=row[2]
+            if 'http' in url:
+                print(no)
+                try:
+                    fun(browser, file, no, name, url)
+                except BaseException as e:
+                    print('BaseException:', e)
+                finally:
+                    print("get success")
+                time.sleep(3)
+    browser.close()
+    file.close()
